bd65583d690fd59728d7385aa1722114990e2f8d
graik <graik@622a1953-5d3b-0410-b9d4-b65186a44f43>
2005-10-24 15:20:05 +0000

    correct copyright statement; add license.txt

--- trunk/Biskit/Statistics/lognormal.py
+++ trunk/Biskit/Statistics/lognormal.py
@@ -1,12 +1,25 @@
 ##
 ## Biskit, a toolkit for the manipulation of macromolecular structures
-## Copyright (c) 2004 Michael Habeck
-## Copyright (c) 2005 Raik Gruenberg
-## All rights reserved
+## Copyright (C) 2004 Michael Habeck
+## Copyright (C) 2005 Raik Gruenberg
 ##
-## Contribution: Michael Habeck, Raik Gruenberg
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or any later version.
 ##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## You find a copy of the GNU General Public License in the file
+## license.txt along with this program; if not, write to the Free
+## Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+## Contributions: Michael Habeck, Raik Gruenberg
 ## $Revision$
+## last $Date$
 ## last $Author$
 
 import Numeric as N
--- trunk/Biskit/hist.py
+++ trunk/Biskit/hist.py
@@ -1,14 +1,26 @@
 ##
 ## Biskit, a toolkit for the manipulation of macromolecular structures
-## Copyright (C) 2004-2005 Wolfgang Rieping; All rights reserved
+## Copyright (C) 2004-2005 Wolfgang Rieping
 ##
-## create a histigram from data
-## 
-## Author: Wolfgang Rieping
+## This program is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or any later version.
 ##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## You find a copy of the GNU General Public License in the file
+## license.txt along with this program; if not, write to the Free
+## Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+##
+
 ## last $Author$
 ## last $Date$
 ## $Revision$
+"""create a histogram from data"""
 
 
 import Numeric
