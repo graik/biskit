087c42cda17f23872d9bcf54aa56187f2b916271
leckner <leckner@622a1953-5d3b-0410-b9d4-b65186a44f43>
2005-11-02 10:39:10 +0000

    Script updated to work with changers in modelling modules.

--- trunk/scripts/Mod/modelling_example.py
+++ trunk/scripts/Mod/modelling_example.py
@@ -1,4 +1,4 @@
-from Biskit.Mod import SequenceSearcher, TemplateSearcher, Aligner, Modeller
+#!/usr/bin/env python
 ##
 ## Biskit, a toolkit for the manipulation of macromolecular structures
 ## Copyright (C) 2004-2005 Raik Gruenberg & Johan Leckner
@@ -18,89 +18,226 @@ from Biskit.Mod import SequenceSearcher, TemplateSearcher, Aligner, Modeller
 ## Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 ##
 ##
+
+from Biskit.Mod import SequenceSearcher
+from Biskit.Mod import TemplateSearcher
+from Biskit.Mod import TemplateCleaner
+from Biskit.Mod import Aligner
+from Biskit.Mod import Modeller
+
+import Biskit.Mod.modUtils as modUtils
 import Biskit.tools as tools
-
-## def test():
-##     options = {}
-##     options['q'] = '/home/Bis/johan/mod_test/target.fasta'
-##     options['o'] = '/home/Bis/johan/mod_test'
-##     return options
+from Biskit import EHandler
+from Biskit import LogFile
 
 
-def test():
+def testOptions():
     options = {}
-    options['q'] = '/home/Bis/oreste/fichier5'
-    options['o'] = '/home/Bis/oreste/fichier5/target.fasta'
+    options['q'] = tools.testRoot()+ '/Mod/project/target.fasta'
+    options['o'] = tools.testRoot()+ '/Mod/project'
+    options['log'] = 'modelling.log'
     return options
 
+def defaultOptions():
+    return {'q':None,
+            'o':'.' }
 
-  
-options = test()
+options = tools.cmdDict( defaultOptions() )
+options = testOptions()
 
-outFolder = outFolder=tools.absfile( options['o'] )
-query_seq = options['q']
+outFolder = tools.absfile( options['o'] )
+f_target = tools.absfile( options['q'] )
+f_target = f_target or outFolder + SequenceSearcher.F_FASTA_TARGET
 
-seq_db = 'sprot.dat'
+log = None
+if 'log' in options:
+    log = LogFile( outFolder + '/' + options['log'] )
+    
+seq_db = 'swissprot'
 tmp_db = 'pdbaa'
 
-###############
-# SequenceSearcher
-
-searcher = SequenceSearcher( outFolder=outFolder, verbose=1 )
-
-## ## remote Blast
-## r = searcher.remoteBlast( options['q'], 'swissprot',
-##                           'blastp', alignments=50 )
-
-## ## remote PSIBlast
-## r = searcher.remotePSIBlast( options['q'], 'swissprot',
-##                              'blastp', alignments=50 )
-
-## ## local PSIBlast
-## rl, el = searcher.localPSIBlast( options['q'], 'pdbaa',
-##                                  'blastpgp', npasses=2)
-
-
-## file name of target sequence
-ftarget = searcher.outFolder + searcher.F_FASTA_TARGET
-
-## local Blast
-searcher.localBlast( ftarget, seq_db, 'blastp', alignments=500, e=0.01 )
-
-searcher.clusterFasta()  ## expects all.fasta
-
-searcher.writeFastaClustered()
-
 
 ###############
-# TemplateSearcher
+## SequenceSearcher
+##
+## Find homologues to the target sequence using blast against "seq_db"
+## Cluster the seuences and write the result to nr.fasta
 
-searcher = TemplateSearcher( outFolder, verbose=1 )
+## input: target.fasta
+## 
+## output: sequences/all.fasta
+##                  /blast.out
+##                  /cluster_result.out
+##                  /nr.fasta                 (input for Aligner)
 
-searcher.localBlast( query_seq, tmp_db, 'blastp', alignments=200, e=0.0001)
+tools.flushPrint('Searching for homologues ...')
 
-searcher.retrievePDBs()
+try:
+    searcher = SequenceSearcher( outFolder=outFolder, verbose=1, log=log )
 
-searcher.clusterFasta()  ## expects all.fasta
+    ## local PSIBlast - not fully implemented!!
+    #searcher.localPSIBlast( target, seq_db, e=0.1, alignments=1000)
 
-searcher.writeFastaClustered()
+    ## local Blast
+    searcher.localBlast( f_target, seq_db, 'blastp', alignments=500, e=0.0001 )
 
-fn = searcher.saveClustered()
+    ## cluster blast results. Defaults: simCut=1.75, lenCut=0.9, ncpu=1
+    ## expects all.fasta
+    searcher.clusterFasta() # simCut=0.45, lenCut=0.9, ncpu=1 )  
 
+    searcher.writeFastaClustered()
+    
+    tools.flushPrint('Done.\n')
+    
+except:
+    EHandler.error( 'Error while searching for homologues.')
+    
 
 ###############
-# Aligner
+## TemplateSearcher
+##
+## Find modelling templates, blasting the target sequence against "tmp_db"
+## Cluster the sequences and download the pdbs to templates/all
 
-a = Aligner( outFolder )
+## input: target.fasta
+##
+## output: templates/blast.out
+##         templates/all.fasta
+##         templates/cluster_result.out
+##         templates/nr.fasta              (input for Aligner)
+##         templates/all/*.pdb
+##         templates/nr/chain_index.txt    (input for TemplateCleaner)
+##                     /*.pdb              (  "    "         "       )
 
-a.align_for_modeller_inp()
+tools.flushPrint('Searching for templates ...')
 
-a.go('scarescrow')
+try:
+    searcher = TemplateSearcher( outFolder, verbose=1 )
+
+    searcher.localBlast(f_target, tmp_db, 'blastp', alignments=200, e=0.001)
+
+    searcher.retrievePDBs()
+
+    ## expects all.fasta
+    searcher.clusterFasta( simCut=1.75, lenCut=0.9, ncpu=1 )  
+
+    searcher.writeFastaClustered()
+
+    fn = searcher.saveClustered()
+
+    tools.flushPrint('Done.\n')
+    
+except:
+    EHandler.error( 'Error while searching for templates')
+
+    
+###################
+## TemplateCleaner
+##
+## Prepare pdb files in templates/nr for T-coffee and modeller
+## (replace nonstandard residues, remove hydrogens,
+#    remove atoms with nultiple configurations, etc.)
+
+## input: templates/nr/*.pdb
+##        templates/nr/chain_index.txt
+##
+## output: templates/t_coffee/*.alpha    (input for Alignar)
+##         templates/modeller/*.pdb      (input for Modeller)
+
+tools.flushPrint('Cleaning template structures...')
+
+try:
+    cleaner = TemplateCleaner( outFolder )
+
+    inp_dic = modUtils.parse_tabbed_file( outFolder +
+                                          TemplateSearcher.F_NR +
+                                          TemplateSearcher.F_CHAIN_INDEX )
+
+    cleaner.process_all( inp_dic )
+
+    tools.flushPrint('Done.\n')
+                     
+except:
+    EHandler.error( 'Error while cleaning templates')
 
 
-###############
-# Aligner
+###################
+## Aligner
+##
+## Create a sequence-structure alignment using T-coffee.
+## Convert the alignment into Modeller compatible format 
 
-m = Modeller( outFolder )
+## input: sequences/nr.fasta
+##        templates/templates.fasta
+##        templates/t_cofee/*.alpha
+##!
+## output: t_coffee/fast_pair.lib
+##                 /final.score_html
+##                 /struct.aln
+##                 /t_coffee.log_*
+##                 /final.aln
+##                 /lalign_id_pair.lib
+##                 /struct.aln_original
+##                 /final.phylip
+##                 /sap_pair.lib
+##                 /t_coffee.inp
+##                 /final.pir_aln             (input for Modeller)
+##                 /sap_pair.lib_original
 
-r = m.prepare_modeller( )
+## note 1: If there are more than approximately 50 sequences overall
+##         t_coffe will eat all the memory and the job will not finish
+
+## note 2: If there is only one template structure step 2 of T-coffee
+##         will not work. Solution, skipp the structural alignment if
+##         only one template structure is provided.
+
+## note 3: In quite som cases the sequence retrieved from the nrpdb
+##         sequence database is different from the sequence extracted
+##         from the coordinates in the pdb-file. This will sometimes
+##         cause t-coffee to terminate with an error (two sequences
+##         with the same name but with different sequences). Temporary
+##         solution: Choose another  structure from the same cluster
+##         as the troublemaker.
+
+tools.flushPrint('Creating sequence/structure alignment...')
+
+try:
+    a = Aligner( outFolder )
+
+    a.align_for_modeller_inp()
+
+    a.go( )
+##    a.go('remotehost') to run on a remote host 
+
+    tools.flushPrint('Done.\n')
+    
+except:
+    EHandler.error( 'Error while building alingnments.')
+
+
+###################
+## Modeller
+##
+## Build model using Modeller.
+
+## input: templates/modeller/*.pdb
+##        t_coffee/final.pir_aln
+##
+## output: modeller/modeller.log
+##                 /*.B9999000??   <- models
+
+tools.flushPrint('Building model...')
+
+try:
+    m = Modeller( outFolder )
+
+    r = m.prepare_modeller( )
+
+    m.go()
+##    a.go('remotehost') to run on a remote host
+    
+    tools.flushPrint('Done.\n')
+    
+except:
+    EHandler.error( 'Error while modelling.')
+
