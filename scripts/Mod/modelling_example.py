33b1f5efd931b2f89447380414c2ba906a5ecea3
leckner <johan@leckner.se>
2005-11-04 13:13:10 +0000

    Added more (better?) documentation.

--- scripts/Mod/modelling_example.py
+++ scripts/Mod/modelling_example.py
@@ -17,7 +17,10 @@
 ## license.txt along with this program; if not, write to the Free
 ## Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 ##
-##
+
+## $Revision$
+## last $Author$
+## last $Date$
 
 from Biskit.Mod import SequenceSearcher
 from Biskit.Mod import TemplateSearcher
@@ -30,20 +33,70 @@ import Biskit.tools as tools
 from Biskit import EHandler
 from Biskit import LogFile
 
+import Biskit.Trajectory as Trajectory
+import Biskit.Pymoler as Pymoler
+import glob
+import sys
+
+
+options = {'q':None,
+           'o':'.'}
+
+def _use():
+    print """
+Biskit.Mod example script that models a structure from a fasta
+formated sequence file in 4 steps:
+
+1) Searches for homologe sequences and clusters the found
+   sequences to a representative set using NCBI-Tools.
+2) Searches for temptale structures for the homology modeling.
+   Similar structures are removed by clustering.
+3) Build a combined sequence/structure alignment using T-Coffee.
+4) Build models using Modeller.
+
+Syntax: modelling_example.py -q |query file| -o |outputFolder|
+                            [-h |host| -log  -view ]
+
+Options:
+   -q     file; fasta formated sequence file to model
+   -o     folder; directory in which all project files will be
+            written
+   -h     host name; the quite cpu consuming stasks of aligning
+            and modeling can be sent to a remote host that also
+            has access to the output directory
+   -log   write stdOut messages to log file (~project/modelling.log)
+   -view  show the superimposed models in PyMol
+
+
+HINT: If you want to instect the alignment used for modeling: 
+      ~project/t_coffee/final.score_html 
+
+
+Default options:"""
+    for key in options.keys():
+        print "\t-",key, "\t",options[key]
+
+    sys.exit(0)
+
 
 def testOptions():
     options = {}
     options['q'] = tools.testRoot()+ '/Mod/project/target.fasta'
     options['o'] = tools.testRoot()+ '/Mod/project'
-    options['log'] = 'modelling.log'
+    options['log'] = '1'
+    options['view'] = '1'
     return options
 
-def defaultOptions():
-    return {'q':None,
-            'o':'.' }
 
-options = tools.cmdDict( defaultOptions() )
-options = testOptions()
+###########################
+# MAIN
+###########################
+
+if len(sys.argv) < 3:
+    _use()
+    
+options = tools.cmdDict( options )
+#options = testOptions()
 
 outFolder = tools.absfile( options['o'] )
 f_target = tools.absfile( options['q'] )
@@ -51,8 +104,10 @@ f_target = f_target or outFolder + SequenceSearcher.F_FASTA_TARGET
 
 log = None
 if 'log' in options:
-    log = LogFile( outFolder + '/' + options['log'] )
-    
+    log = LogFile( outFolder + '/modelling.log'  )
+
+
+## databases used
 seq_db = 'swissprot'
 tmp_db = 'pdbaa'
 
@@ -73,17 +128,18 @@ tmp_db = 'pdbaa'
 tools.flushPrint('Searching for homologues ...')
 
 try:
+    # initiate 
     searcher = SequenceSearcher( outFolder=outFolder, verbose=1, log=log )
 
-    ## local PSIBlast - not fully implemented!!
-    #searcher.localPSIBlast( target, seq_db, e=0.1, alignments=1000)
+#    ## local PSIBlast - not fully implemented!!
+#    searcher.localPSIBlast( target, seq_db, e=0.1, alignments=1000)
 
     ## local Blast
     searcher.localBlast( f_target, seq_db, 'blastp', alignments=500, e=0.0001 )
 
     ## cluster blast results. Defaults: simCut=1.75, lenCut=0.9, ncpu=1
     ## expects all.fasta
-    searcher.clusterFasta() # simCut=0.45, lenCut=0.9, ncpu=1 )  
+    searcher.clusterFasta() 
 
     searcher.writeFastaClustered()
     
@@ -112,7 +168,7 @@ except:
 tools.flushPrint('Searching for templates ...')
 
 try:
-    searcher = TemplateSearcher( outFolder, verbose=1 )
+    searcher = TemplateSearcher( outFolder, verbose=1, log=log )
 
     searcher.localBlast(f_target, tmp_db, 'blastp', alignments=200, e=0.001)
 
@@ -147,7 +203,7 @@ except:
 tools.flushPrint('Cleaning template structures...')
 
 try:
-    cleaner = TemplateCleaner( outFolder )
+    cleaner = TemplateCleaner( outFolder, log=log )
 
     inp_dic = modUtils.parse_tabbed_file( outFolder +
                                           TemplateSearcher.F_NR +
@@ -170,7 +226,7 @@ except:
 ## input: sequences/nr.fasta
 ##        templates/templates.fasta
 ##        templates/t_cofee/*.alpha
-##!
+##
 ## output: t_coffee/fast_pair.lib
 ##                 /final.score_html
 ##                 /struct.aln
@@ -202,12 +258,14 @@ except:
 tools.flushPrint('Creating sequence/structure alignment...')
 
 try:
-    a = Aligner( outFolder )
+    a = Aligner( outFolder, log=log )
 
     a.align_for_modeller_inp()
 
-    a.go( )
-##    a.go('remotehost') to run on a remote host 
+    if options.has_key('host'):
+        a.go(options['host'])
+    else:        
+        a.go( )
 
     tools.flushPrint('Done.\n')
     
@@ -229,15 +287,68 @@ except:
 tools.flushPrint('Building model...')
 
 try:
-    m = Modeller( outFolder )
+    m = Modeller( outFolder, log=log )
 
     r = m.prepare_modeller( )
 
-    m.go()
-##    a.go('remotehost') to run on a remote host
-    
+    if options.has_key('host'):
+        m.go(options['host']) 
+    else:
+        m.go()
+        
     tools.flushPrint('Done.\n')
     
 except:
     EHandler.error( 'Error while modelling.')
 
+
+
+###################
+##
+## Superimpose the models and look at the result in Pymol
+##
+
+def __printMatrix( matrix ):
+    """
+    Print the part right of the diagonal in a matrix
+    """
+    nr = len( matrix )
+    for i in range(nr): print '%5i'%(i+1),
+    for i in range(nr):
+        print '\n%2i'%(i+1),
+        for k in range(i):
+            print ' '*5,
+        for j in range(i, nr):
+            print '%5.2f'%matrix[i,j],
+            
+    
+## get filenames of all models 
+models = glob.glob( '%s/modeller/%s*.pdb'%(outFolder,
+                                           tools.stripFilename(f_target)) )
+
+## create a Trajectory object with the models
+traj = Trajectory( pdbs=models )
+
+## fit the models against the average structure iteratively
+traj.blockFit2ref()
+
+## calculate and print rmsd matrix
+rmsHeavy = traj.pairwiseRmsd()
+print '\nHEAVY ATOM RMSD BETWEEN MODELS::'
+__printMatrix( rmsHeavy )
+
+## same thing for backbone atoms
+BBMask = traj[0].maskBB()
+traj.blockFit2ref( mask = BBMask )
+rmsBB = traj.pairwiseRmsd( aMask = BBMask  )
+print '\nBACKBONE RMSD BETWEEN MODELS:'
+__printMatrix( rmsBB )
+
+if options.has_key('view'):
+    
+    ## show backbone superimposed structures in pymol
+    pm = Pymoler( )
+    for t in traj:
+        pm.addPdb( t )
+
+    pm.show()
