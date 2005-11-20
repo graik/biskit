902e35189d331d65babd45731824ac08903cb434
graik <graik@622a1953-5d3b-0410-b9d4-b65186a44f43>
2005-11-20 10:06:28 +0000

    move color palette management from MatrixPlot into separate ColorSpectrum class

--- trunk/Biskit/MatrixPlot.py
+++ trunk/Biskit/MatrixPlot.py
@@ -21,9 +21,9 @@
 ## $Revision$
 """Plot a 2D matrix (up to 100 x 100)"""
 
-
 import biggles
 import Numeric as N
+import ColorSpectrum as C 
 
 class Legend(biggles.FramedPlot):
 
@@ -64,33 +64,19 @@ class Legend(biggles.FramedPlot):
 
 class MatrixPlot(biggles.FramedPlot):
 
-    max_col = {'grey': 3 * 255,
-               'plasma': 3 * 255,
-               'plasma2': 3 * 255,
-               'sausage': 2 * 255}
-
     def __init__(self, matrix, mesh = 0, palette = "plasma", legend = 0):
 
         biggles.FramedPlot.__init__(self)
 
-        f_pal = {'grey': self.__grey,
-                 'plasma': self.__plasma,
-                 'plasma2': self.__plasma2,
-                 'sausage': self.__sausage}
+        self.palette = C.ColorSpectrum( palette )
 
-        self.palette = palette
-        self.f_pal = f = f_pal[palette]
-
-        self.min, self.max, matrix = self.__normalize(matrix)
-        self.m = matrix
-        s = N.shape(matrix)
+        self.matrix = self.palette.color_array( matrix )
+        s = N.shape( self.matrix )
 
         for i in range(s[0]):
             for j in range(s[1]):
 
-                x = int(round(matrix[i,j]))
-
-                col = self.__make_col(*f(x))
+                col = self.matrix[i,j]
 
                 x1 = (j, j + 1)
                 y1 = (i, i)
@@ -118,117 +104,17 @@ class MatrixPlot(biggles.FramedPlot):
 
         self.aspect_ratio = 1.0
         
+
     def __make_legend(self):
 
-        max_col = self.max_col[self.palette]
+        l = self.palette.legend()
 
-        step = (self.max - self.min) / max_col
-        values = N.arange(self.min, self.max + step, step)
-
-        l = []
-
-        for i in range(max_col):
-            col = self.__make_col(*self.f_pal(i))
-            l.append((values[i], col))
-
-        legend = Legend(l)
+        legend = Legend( l )
 
         inset = biggles.Inset((1.1, 0.60), (1.2, .97), legend)
 
         return inset
 
-    def __normalize(self, matrix):
-
-        _max = max(N.ravel(matrix))
-        _min = min(N.ravel(matrix))
-        
-        s = N.shape(matrix)
-
-        max_col = self.max_col[self.palette]
-
-        matrix = (matrix - _min) * max_col / (_max - _min)
-
-        return _min, _max, matrix
-
-    def __map(self, x):
-        return int(min(255, round(x)))
-
-    def __make_col(self, red, green, blue):
-        return ((red << 16) + (green << 8) + blue)        
-
-    def __grey(self, x):
-        x = self.__map(255 * x / self.max_col['grey'])
-        return x, x, x
-
-    def __plasma2(self, x):
-
-        blue_range = 150
-        red_range = 255
-        green_range = 255
-        
-        if x <= blue_range:
-            red = 0
-            green = self.__map(x)
-            blue = self.__map(blue_range - x)
-
-        elif x <= 255:
-            red = 0
-            blue = 0
-            green = self.__map(x)
-
-        elif x > 255 + green_range:
-            x -= 255 + green_range
-            blue = 0
-            red = 255
-            green = self.__map(255 - x)
-        else:
-            x -= 255
-            blue = 0
-            red = self.__map(x)
-            green = 255
-
-        return red, green, blue
-
-    def __plasma(self, x):
-
-        blue_range = 255
-        red_range = 255
-        green_range = 255
-        
-        if x <= blue_range:
-            red = 0
-            green = self.__map(x)
-            blue = self.__map(blue_range - x)
-
-        elif x > 255 + green_range:
-            x -= 255 + green_range
-            blue = 0
-            red = 255
-            green = self.__map(255 - x)
-        else:
-            x -= 255
-            blue = 0
-            red = self.__map(x)
-            green = 255
-
-        return red, green, blue
-
-    def __sausage(self, x):
-
-        first_half = 255
-
-        if x <= first_half:
-            red = self.__map(x)
-            green = 0
-            blue = self.__map(first_half - x)
-
-        else:
-            x -= 255
-            red = 255
-            green = self.__map(x)
-            blue = int(0.3 * 255)
-
-        return red, green, blue
     
 if __name__ == '__main__':
 
@@ -242,5 +128,5 @@ if __name__ == '__main__':
         for j in range(shape(z)[1]):
             z[i,j] = exp(-0.01*((i-n/2)**2+(j-n/2)**2))
 
-    p = MatrixPlot(z, palette='sausage', legend = 1)
+    p = MatrixPlot(z, palette='sausage', legend=1)
     p.show()
