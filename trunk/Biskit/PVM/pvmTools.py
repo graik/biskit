354de705521ca68a4141bdb3ec750ec9a40a9f6e
graik <graik@622a1953-5d3b-0410-b9d4-b65186a44f43>
2006-11-16 15:42:30 +0000

    dictionary for pvm error codes

--- trunk/Biskit/PVM/pvmTools.py
+++ trunk/Biskit/PVM/pvmTools.py
@@ -75,3 +75,41 @@ def countHosts():
     @rtype: int
     """
     return PVM.pypvm_core.config()[0]
+
+## Error codes returned by pvmlib
+pvmerrors = {
+    0  : 'PvmOk	     Success',
+    -2 : 'PvmBadParam     Bad parameter',
+    -3 : 'PvmMismatch     Parameter mismatch',
+    -4 : 'PvmOverflow     Value too large',
+    -5 : 'PvmNoData       End of buffer',
+    -6 : 'PvmNoHost       No such host',
+    -7 : 'PvmNoFile       No such file',
+    -8 : 'PvmDenied       Permission denied',
+    -10: 'PvmNoMem        Malloc failed',
+    -12: "PvmBadMsg       Can't decode message",
+    -14: "PvmSysErr       Can't contact local daemon",
+    -15: 'PvmNoBuf        No current buffer',
+    -16: 'PvmNoSuchBuf    No such buffer',
+    -17: 'PvmNullGroup    Null group name',
+    -18: 'PvmDupGroup     Already in group',
+    -19: 'PvmNoGroup      No such group',
+    -20: 'PvmNotInGroup   Not in group',
+    -21: 'PvmNoInst       No such instance',
+    -22: 'PvmHostFail     Host failed',
+    -23: 'PvmNoParent     No parent task',
+    -24: 'PvmNotImpl      Not implemented',
+    -25: 'PvmDSysErr      Pvmd system error',
+    -26: 'PvmBadVersion   Version mismatch',
+    -27: 'PvmOutOfRes     Out of resources',
+    -28: 'PvmDupHost      Duplicate host',
+    -29: "PvmCantStart    Can't start pvmd",
+    -30: 'PvmAlready      Already in progress',
+    -31: 'PvmNoTask       No such task',
+    -32: 'PvmNotFound     Not Found',
+    -33: 'PvmExists       Already exists',
+    -34: 'PvmHostrNMstr   Hoster run on non-master host',
+    -35: 'PvmParentNotSet Spawning parent set PvmNoSpawnParent',
+    -36: "PvmIPLoopback   Master Host's IP is Loopback"
+}
+    
