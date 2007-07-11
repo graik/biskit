b73c2c50d8d9695f24a8fa4ce6f032698f2008d5
graik <raik.gruenberg@gmail.com>
2007-07-11 16:52:06 +0000

    bugfix last change

--- scripts/Mod/model.py
+++ scripts/Mod/model.py
@@ -182,7 +182,7 @@ try:
     m8 = M( **options )
    
     if not 'dry' in options:
-	r = m8.run( options['host'] )  ## comment out for testing
+	r = m8.run()  ## comment out for testing
 
 except:
     EHandler.error( 'Error while modelling.')
