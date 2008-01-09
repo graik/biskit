ca3f55112c2d6525cb6a5ca3aa6c2714386305ba
graik <raik.gruenberg@gmail.com>
2008-01-09 14:44:12 +0000

    bugfix 1764217 and 1866886

--- Biskit/Mod/SequenceSearcher.py
+++ Biskit/Mod/SequenceSearcher.py
@@ -703,7 +703,8 @@ class SequenceSearcher:
         """
         iter = 1
         while self.clustersCurrent > self.clusterLimit \
-              or self.clustersCurrent == None:
+              or self.clustersCurrent == None \
+              or simCut <= 0 or lenCut <= 0:
 
             self.clusterFasta( fastaIn, simCut, lenCut, ncpu )
             self.clustersCurrent = len( self.clusters )
