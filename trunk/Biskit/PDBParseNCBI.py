c34d92ddebc5131e0a0c9b302d57893a0384d330
graik <graik@622a1953-5d3b-0410-b9d4-b65186a44f43>
2009-07-10 13:19:15 +0000

    adapt pdb file fetching to flatfile folder database

--- trunk/Biskit/PDBParseNCBI.py
+++ trunk/Biskit/PDBParseNCBI.py
@@ -90,7 +90,7 @@ class PDBParseNCBI( PDBParseModel ):
         @raise PDBParserError: if couldn't find PDB file
         """
         id = string.lower( id )
-        filenames = ['%s.pdb' % id,
+        filenames = [os.path.join( db_path, '%s.pdb' % id),
                      db_path + '/pdb%s.ent' % id,
                      db_path + '/%s/pdb%s.ent.Z' %( id[1:3], id ) ]
 
