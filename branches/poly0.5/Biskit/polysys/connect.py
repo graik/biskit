a4d866b39da95ba5a9581e95bada67637bfe4b17
graik <graik@622a1953-5d3b-0410-b9d4-b65186a44f43>
2009-07-31 18:45:01 +0000

    work in progress

--- branches/poly0.5/Biskit/polysys/connect.py
+++ branches/poly0.5/Biskit/polysys/connect.py
@@ -22,8 +22,10 @@
 ## $Revision: 810 $
 
 import os.path as osp
-from Biskit import PDBModel
+from Biskit import PDBModel, BiskitError, PDBError
 import Biskit.tools as T
+from Biskit.Residue import Residue ## experimental
+import numpy as N
 
 class FusionError( BiskitError ):
     pass
@@ -47,26 +49,79 @@ class Fusion( object ):
                         custom PDBModel with two residues.
         @type  adapter: str OR PDBModel
         """
+        try:
+            if type( adapter ) is str and not osp.isfile( adapter ) :
+                self.adapter_model = PDBModel( self.adapters[ adapter ] )
+            elif type( adapter ) is str and osp.isfile( adapter ):
+                self.adapter_model = PDBModel( adapter )
+            elif isinstance( adapter, PDBModel ):
+                self.adapter_model = adapter
+            else:
+                raise FusionError, 'invalid adapter argument: %r' % adapter
 
-        if type( adapter ) is str and not os.path.isfile( adapter ) :
-            self.adapter_model = PDBModel( self.adapters[ adapter ] )
-        elif type( adapter ) is str and os.path.isfile( adapter ):
-            self.adapter_model = PDBModel( adapter )
-        elif isinstance( adapter, PDBModel ):
-            self.adapter_model = adapter
-        else:
-            raise FusionError, 'invalid adapter argument: %r' % adapter
+        except PDBError, why:
+            raise FusionError, 'cannot load adapter: ' + why
     
+    def select_atoms( self, m, rindex=0, names=[''] ):
+        """
+        Select atoms within a single residue
+        """
+        rmask = N.zeros( m.lenResidues() )
+        rmask[rindex] = 1
+        
+        amask = N.zeros( m.lenAtoms() )
+        for atom in names:
+            amask += m['name'] == atom
+
+        return amask * m.res2atomMask( rmask )
+       
+        
+    def remove_atoms( self, m, rindex=-1, names=['O', 'OT1', 'OT2', 'OXT'] ):
+        """
+        Remove atoms with given name from given residue.
+        """     
+        amask = self.select_atoms( m, rindex, names )
+        m.remove( N.flatnonzero( amask ) )
+
+        
     def fuse( self, m1, m2 ):
         """
         """
-        rmask = N.zeros( m1.lenResidues() )
-        rmask[-1] = 1
+        m1 = m1.clone()
+        m2 = m2.clone()
         
-        amask = N.zeros( m1.lenAtoms() )
-        for atom in ['O', 'OT1', 'OT2', 'OXT']:
-            amask += m1['name'] == atom
-
-        amask = 
+        self.remove_atoms( m1, rindex=-1, names=['O', 'OT1', 'OT2', 'OXT'] )
+        self.remove_atoms( m2, rindex=0,  names=['HT1', 'HT2', 'HT3'] )
         
-        return m1.concat( m2 )
+        ## fit adapter to C-terminal of model 1
+        mask_1  = self.select_atoms( m1, rindex=-1, names=['N', 'CA', 'C'] )
+        mask_adapt = self.select_atoms( self.adapter_model, 
+                                   rindex=0, names=['N', 'CA', 'C'] )
+        
+        ref = m1.compress( mask_1 )
+        
+        assert( isinstance( self.adapter_model, PDBModel ) ) 
+        self.adapter_model.fit( ref, mask=mask_adapt ) 
+        
+        ## fit N terminal model2 to C terminal of adapter        
+        mask_2 = self.select_atoms( m2, rindex=0, names=['N', 'CA', 'C'] )
+        mask_adapt = self.select_atoms( self.adapter_model, 
+                                        rindex=1, names=['N', 'CA', 'C'] )
+        
+        ref = self.adapter_model.compress( mask_adapt )
+        
+        m2.fit( ref, mask=mask_2 )
+        
+        ## concatenate models
+        assert( isinstance( m1, PDBModel ) )
+        r = m1.concat( m2 )
+        r['chain_id'] = r['chain_id'][0] * len( r )
+        
+        return r
+    
+if __name__ == '__main__':
+    
+    m1 = PDBModel( T.testRoot( 'polysys/spectrinr16.pdb' ) )
+    
+    f = Fusion( adapter='helix' )
+    r = f.fuse( m1, m1 )
