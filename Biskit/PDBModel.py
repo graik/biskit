31a5b18fa0e1827bd35b2d6c0e91890b78bc86b7
graik <raik.gruenberg@gmail.com>
2012-02-23 01:10:12 +0000

    bugfix 3489814: revert break residue to atom assignment in chainBreaks() to older version

--- Biskit/PDBModel.py
+++ Biskit/PDBModel.py
@@ -2955,17 +2955,19 @@ class PDBModel:
                 r = N.nonzero( N.greater( dist, cutoff ) )
 
             if len(r) > 0:
-    
+                
+                ## can probably be simplified with self.resEndIndex()
                 ri = self.resIndex()
-##                ri_to_e = {}
-##                for i in range( len(ri)-1 ):
-##                    ri_to_e[ ri[i] ] = ri[ i+1 ]-1
-##    
-##                ## map back to the original atom indices
-##                r = [ ri_to_e[ i_bb[ bb_ri[i] ] ] for i in r ]
-                # can probably be replaced by this (not extensively tested):
-                re = self.resEndIndex()
-                r = [ re[i] for i in r ]
+                ri_to_e = {}
+                for i in range( len(ri)-1 ):
+                    ri_to_e[ ri[i] ] = ri[ i+1 ]-1
+    
+                ## map back to the original atom indices
+                r = [ ri_to_e[ i_bb[ bb_ri[i] ] ] for i in r ]
+
+                ## this replacement didn't work out (fails PDBCleaner testcase):
+                #re = self.resEndIndex()
+                #r = [ re[i] for i in r ]
 
 
             if breaks_only:
