ad978382ae356994158cef143a4f9f863d647fa7
graik <graik@users.noreply.github.com>
2016-06-09 00:15:13 +0300

    fine tune

--- __analyze_oldnumeric.py
+++ __analyze_oldnumeric.py
@@ -12,18 +12,17 @@ for l in lines:
     method = None
     try:
         method = ex.findall(l)[0]
+        
+        if not method in r:
+            r[method] = 0
+
+        r[method] += 1
     except:
         print 'could not match: ', l,
-
-    if method and not method in r:
-        r[method] = 0
-
-    if method:
-        r[method] += 1
 
 print 'RESULT'
 print
 for method, count in r.items():
     print "%15s: %3i" % (method, count)
 
-print 'found: ', sum([c for m,c in r.items()])
+print 'found: ', sum([c for m,c in r.items()]), ' calls of ', len(r), ' different methods'
