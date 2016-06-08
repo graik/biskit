90a84884d4b051f8ac1e3183fa37b8ee957bffe9
graik <graik@users.noreply.github.com>
2016-06-08 23:45:16 +0300

    quick analysis of oldnumeric usage

---
+++ __analyze_oldnumeric.py
@@ -0,0 +1,27 @@
+import re
+
+f = open('__listoldnumeric_uses.txt')
+
+lines = f.readlines()
+
+r = {}
+
+ex = re.compile('oldN\.([a-z]+)[\( ].+')
+
+for l in lines:
+    method = None
+    try:
+        method = ex.findall(l)[0]
+    except:
+        print 'could not match: ', l
+
+    if method and not method in r:
+        r[method] = 0
+
+    if method:
+        r[method] += 1
+
+print 'RESULT'
+print
+for method, count in r.items():
+    print "%15s: %2i" % (method, count)
