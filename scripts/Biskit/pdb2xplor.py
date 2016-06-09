249f3a9b615d9d3502d11ee8fdfd43690d820882
graik <graik@users.noreply.github.com>
2016-06-09 22:36:42 +0300

    remove last references to numpy.oldnumeric

--- scripts/Biskit/pdb2xplor.py
+++ scripts/Biskit/pdb2xplor.py
@@ -35,7 +35,10 @@ from Biskit import Executor
 from Biskit import settings
 from Biskit import Pymoler
 
-from numpy.oldnumeric import *
+## from numpy.oldnumeric import *
+## migration not tested (!!):
+from Biskit.oldnumeric import *
+
 from string import *
 import sys        # sys.exc_type, os.abspath
 import re, os, os.path
